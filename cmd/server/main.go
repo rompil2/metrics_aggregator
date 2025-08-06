4935dbb06448a9c638de628185545a749ce7d4a5
Roman <Rompil@inbox.ru>
2025-08-06 15:52:57 +0300

    add sigterm support for a server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/rompil2/metrics_aggregator/internal/handler"
 	"github.com/rompil2/metrics_aggregator/internal/repository"
@@ -19,7 +24,34 @@ func main() {
 
 	handler := handler.NewHandlerMux(&srvc)
 
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		log.Println(err)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
 	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		log.Println("The server is starting...")
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-done
+	log.Println("The server is shuting down...")
+	// Create a context with timeout for shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Attempt graceful shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Graceful shutdown failed: %v\n", err)
+		if err := server.Close(); err != nil {
+			log.Fatalf("Forced shutdown failed: %v\n", err)
+		}
+	}
+
+	log.Println("Server stopped")
+
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,13 @@ module github.com/rompil2/metrics_aggregator
 
 go 1.23.2
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/golang/mock v1.6.0
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/mock v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/ramya-rao-a/go-outline v0.0.0-20210608161538-9736a4bde949 // indirect
-	golang.org/x/tools v0.35.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
