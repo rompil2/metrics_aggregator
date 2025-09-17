0adfefa79d9e1f7d3840a66f97be870ed0a300f1
Roman <Rompil@inbox.ru>
2025-09-18 00:36:36 +0300

    revert prev changes

--- internal/repository/dbstore/dbstore.go
+++ internal/repository/dbstore/dbstore.go
@@ -2,31 +2,29 @@ package dbstore
 
 import (
 	"database/sql"
-	"fmt"
-	"time"
 
 	"errors"
 	"log"
 
-	"github.com/jackc/pgerrcode"
-	"github.com/jackc/pgx/v5/pgconn"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
 	"github.com/rompil2/metrics_aggregator/internal/model"
 	"github.com/rompil2/metrics_aggregator/migrations"
 )
 
-const maxAttempts = 3
-const updateCounterSQL = `
-		INSERT INTO metrics (id, m_type, delta, value, hash)
-		VALUES ($1, $2, $3, NULL, $4)
-		ON CONFLICT (id) 
-		DO UPDATE SET 
-			delta = metrics.delta + EXCLUDED.delta,
-			hash = EXCLUDED.hash,
-			updated_at = CURRENT_TIMESTAMP
-	`
-const updateGaugeSQL = `
+var db *sql.DB
+
+const (
+	updateCounterSQL = `
+			INSERT INTO metrics (id, m_type, delta, value, hash)
+			VALUES ($1, $2, $3, NULL, $4)
+			ON CONFLICT (id) 
+			DO UPDATE SET 
+				delta = metrics.delta + EXCLUDED.delta,
+				hash = EXCLUDED.hash,
+				updated_at = CURRENT_TIMESTAMP
+		`
+	updateGaugeSQL = `
 		INSERT INTO metrics (id, m_type, delta, value, hash)
 		VALUES ($1, $2, NULL, $3, $4)
 		ON CONFLICT (id) 
@@ -35,20 +33,22 @@ const updateGaugeSQL = `
 			hash = EXCLUDED.hash,
 			updated_at = CURRENT_TIMESTAMP
 	`
-
-var retryDelays = []time.Duration{1 * time.Second, 3 * time.Second, 5 * time.Second}
+	allMetricsSQL = `SELECT id, m_type, delta, value, hash, updated_at FROM metrics ORDER BY updated_at DESC`
+)
 
 type DBStore struct {
 	db *sql.DB
 }
 
-type Void struct{}
-
-func NewDBStore(db *sql.DB) (DBStore, error) {
+func NewDBStore(connStr string) (DBStore, error) {
 	if db != nil {
 		return DBStore{db}, nil
 	}
 
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		return DBStore{}, err
+	}
 	dbs := DBStore{db}
 	// Check connection
 	if err := dbs.Ping(); err != nil {
@@ -122,24 +122,14 @@ func (s DBStore) SetAllMetrics(metrics []model.Metrics) error {
 }
 
 func (s DBStore) Ping() error {
-	return s.withRetry(func() error {
-		return s.db.Ping()
-	})
+	return s.db.Ping()
 }
 
 func (s DBStore) SetMetrics(ID string, metric model.Metrics) error {
-	return s.withRetry(func() error {
-		return s.setMetrics(ID, metric)
-	})
-}
-
-func (s DBStore) setMetrics(ID string, metric model.Metrics) error {
 	if metric.MType == "counter" && metric.Delta != nil {
-
 		_, err := s.db.Exec(updateCounterSQL, ID, metric.MType, *metric.Delta, metric.Hash)
 		return err
 	} else if metric.MType == "gauge" && metric.Value != nil {
-
 		_, err := s.db.Exec(updateGaugeSQL, ID, metric.MType, *metric.Value, metric.Hash)
 		return err
 	}
@@ -148,17 +138,6 @@ func (s DBStore) setMetrics(ID string, metric model.Metrics) error {
 }
 
 func (s DBStore) GetMetrics(metricID string) (model.Metrics, error) {
-	var result model.Metrics
-	var err error
-	err = s.withRetry(func() error {
-		result, err = s.getMetrics(metricID)
-		return err
-	})
-
-	return result, err
-}
-
-func (s DBStore) getMetrics(metricID string) (model.Metrics, error) {
 	var metric model.Metrics
 	var delta sql.NullInt64
 	var value sql.NullFloat64
@@ -192,20 +171,8 @@ func (s DBStore) getMetrics(metricID string) (model.Metrics, error) {
 }
 
 func (s DBStore) GetAllMetrics() ([]model.Metrics, error) {
-	var result []model.Metrics
-	var err error
-	err = s.withRetry(func() error {
-		result, err = s.getAllMetrics()
-		return err
-	})
 
-	return result, err
-}
-
-func (s DBStore) getAllMetrics() ([]model.Metrics, error) {
-	query := `SELECT id, m_type, delta, value, hash, updated_at FROM metrics ORDER BY updated_at DESC`
-
-	rows, err := s.db.Query(query)
+	rows, err := s.db.Query(allMetricsSQL)
 	if err != nil {
 		return nil, err
 	}
@@ -269,79 +236,4 @@ func (s DBStore) Migrate() error {
 
 func (s DBStore) Close() error {
 	return s.db.Close()
-}
-
-// Error retry wrapper
-func (s *DBStore) withRetry(operation func() error) error {
-
-	var lastErr error
-
-	for attempt := range maxAttempts {
-		err := operation()
-		if err == nil {
-			return nil
-		}
-
-		lastErr = err
-
-		if !s.isRetriableError(err) {
-			return err
-		}
-
-		if attempt < maxAttempts-1 {
-			log.Printf("Database operation failed (attempt %d/%d), retrying in %v: %v",
-				attempt+1, maxAttempts, retryDelays[attempt], err)
-
-			select {
-			case <-time.After(retryDelays[attempt]):
-				// Continue with next attempt
-			default:
-			}
-		}
-	}
-
-	return fmt.Errorf("all %d attempts failed, last error: %w", maxAttempts, lastErr)
-}
-
-func (s *DBStore) isRetriableError(err error) bool {
-	if err == nil {
-		return false
-	}
-
-	// PostgreSQL error check
-	var pgErr *pgconn.PgError
-	if errors.As(err, &pgErr) {
-		// Retriable
-		retriableCodes := map[string]Void{
-			pgerrcode.ConnectionException:                     {},
-			pgerrcode.ConnectionDoesNotExist:                  {},
-			pgerrcode.ConnectionFailure:                       {},
-			pgerrcode.TransactionRollback:                     {},
-			pgerrcode.SerializationFailure:                    {},
-			pgerrcode.DeadlockDetected:                        {},
-			pgerrcode.CannotConnectNow:                        {},
-			pgerrcode.AdminShutdown:                           {},
-			pgerrcode.CrashShutdown:                           {},
-			pgerrcode.TooManyConnections:                      {},
-			pgerrcode.InvalidAuthorizationSpecification:       {},
-			pgerrcode.SQLClientUnableToEstablishSQLConnection: {},
-		}
-
-		if _, exist := retriableCodes[pgErr.Code]; exist {
-			return true
-		}
-
-	}
-
-	// Network errors are retriable
-	var netErr interface {
-		Timeout() bool
-		Temporary() bool
-	}
-	if errors.As(err, &netErr) && (netErr.Timeout() || netErr.Temporary()) {
-		return true
-	}
-
-	// Non-retriable by default
-	return false
 }
